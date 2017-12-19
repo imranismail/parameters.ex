dfe8b689035f756e51d6de759c17878e172aaca4
Imran Ismail <imran.codely@gmail.com>
2017-12-20 00:47:55 +0800

    Prep for release

--- mix.exs
+++ mix.exs
@@ -7,8 +7,24 @@ defmodule Parameters.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
+  end
+
+  defp description do
+    """
+    An easy way to create Ecto Schemas for API endpoint validations
+    """
+  end
+
+  defp package do
+    [name: :parameters,
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Imran Ismail"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/imranismail/parameters.ex"}]
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -21,7 +37,8 @@ defmodule Parameters.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 2.2"}
+      {:ecto, "~> 2.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
