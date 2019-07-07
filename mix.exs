de5645f64212c1107487ca94aaf63faa4d3ddf9a
Imran Ismail <imran.codely@gmail.com>
2019-07-07 21:08:24 +0800

    Add tests

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Parameters.Mixfile do
       app: :parameters,
       version: "1.0.0",
       elixir: "~> 1.5",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
@@ -35,6 +36,10 @@ defmodule Parameters.Mixfile do
       extra_applications: [:logger]
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
--- test/parameters_test.exs
+++ test/parameters_test.exs
@@ -1,33 +1,8 @@
 defmodule ParametersTest do
-  use ExUnit.Case
+  use Parameters.Case
+  use Parameters
+
   doctest Parameters
-
-  defmodule ControllerMock do
-    use Parameters
-
-    params do
-      requires :root_req, :string, default: "required"
-      optional :root_opt, :integer
-
-      requires :profile, :map do
-        requires :in_group_req, :string
-        optional :in_group_opt, :string
-
-        optional :tendencies, :array do
-          optional :name, :string
-          optional :dunno, :string
-        end
-      end
-
-      requires :profiles, :array do
-        requires :oneline, :string
-      end
-    end
-
-    def create(conn, _params) do
-      conn
-    end
-  end
 
   @valid_params %{
     root_opt: 1,
@@ -42,13 +17,70 @@ defmodule ParametersTest do
 
   @invalid_params %{}
 
-  test "Schema.params_for/1" do
-    conn = %{
-      params: nil,
-      private: %{phoenix_controller: ParametersTest.ControllerMock, phoenix_action: :create}
-    }
+  params do
+    requires :root_req, :string, default: "required"
+    optional :root_opt, :integer
+
+    requires :profile, :map do
+      requires :in_group_req, :string
+      optional :in_group_opt, :string
+
+      optional :tendencies, :array do
+        optional :name, :string
+        optional :dunno, :string
+      end
+    end
+
+    requires :profiles, :array do
+      requires :oneline, :string
+    end
+  end
+
+  def controller_action_mock(conn, _params) do
+    conn
+  end
+
+  params do
+    requires :nope_nope, :string
+  end
+
+  def another_controller_action_mock(conn, _params) do
+    conn
+  end
+
+  test "Parameters are namespaced" do
+    assert not module_defined?(ParametersTest.ControllerActionMock)
+    assert module_defined?(Parameters.ParametersTest.ControllerActionMock)
+  end
+
+  test "Parameters are defined per action" do
+    assert module_defined?(Parameters.ParametersTest.AnotherControllerActionMock)
+  end
+
+  test "Parameters should be nestable like Ecto's inline embeds" do
+    assert module_defined?(Parameters.ParametersTest.ControllerActionMock.Profile)
+    assert module_defined?(Parameters.ParametersTest.ControllerActionMock.Profile.Tendencies)
+    assert module_defined?(Parameters.ParametersTest.ControllerActionMock.Profiles)
+  end
+
+  test "Schema.params_for/1 should validate parameters" do
+    conn = phoenix_conn(ParametersTest, :controller_action_mock)
 
     assert {:ok, params} = Parameters.params_for(%{conn | params: @valid_params})
+    assert {:error, %Ecto.Changeset{}} = Parameters.params_for(%{conn | params: @invalid_params})
+  end
+
+  test "Schema.changeset_for/1 should return changeset" do
+    conn = phoenix_conn(ParametersTest, :controller_action_mock)
+
+    assert %Ecto.Changeset{} = Parameters.changeset_for(%{conn | params: @valid_params})
+  end
+
+  test "Schemas are different for each action" do
+    conn = phoenix_conn(ParametersTest, :controller_action_mock)
+    assert {:ok, params} = Parameters.params_for(%{conn | params: @valid_params})
+
+    conn = phoenix_conn(ParametersTest, :another_controller_action_mock)
     assert {:error, %Ecto.Changeset{}} = Parameters.params_for(%{conn | params: @invalid_params})
   end
 end
